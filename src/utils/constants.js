b76828d44dc3e4680954cf46d42580fd3f22c7a7
Sai-Kokate <saikokatelearning@gmail.com>
2023-05-11 12:20:22 +0530

    updating categories

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,8 +16,8 @@ export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
-  { name: "Coding", icon: <CodeIcon /> },
+  { name: "Frontend Development", icon: <CodeIcon /> },
+  { name: "MERN Stack", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
   { name: "Music", icon: <MusicNoteIcon /> },
